f2abd263d7dbe5194f4c78cbbac6fcbf668b368d
Lebohang Mokhwibitjane <lebohangmokhwibitjane@gmail.com>
2025-05-06 03:34:52 +0200

    fix:switch to typescript

---
+++ dist/main.js
@@ -0,0 +1,3 @@
+"use strict";
+const canvas = document.querySelector(".canvas");
+const ctx = canvas === null || canvas === void 0 ? void 0 : canvas.getContext("2d");
---
+++ src/main.ts
@@ -0,0 +1,3 @@
+
+const canvas = document.querySelector<HTMLCanvasElement>(".canvas");
+const ctx = canvas?.getContext("2d");
